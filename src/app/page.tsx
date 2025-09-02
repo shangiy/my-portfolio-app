c8c04a3662a89d6b3f5e5488787ab916b2320c8b
shangi Shangstone <patrickshangstone22@gmail.com>
2025-09-02 10:59:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,13 @@ import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { ScrollToTopButton } from '@/components/ui/scrollToTopButton';
 
-export default function Home({}: {}) {
+export default function Home({
+  params,
+  searchParams,
+}: {
+  params?: object;
+  searchParams?: object;
+}) {
   const [activeLink, setActiveLink] = useState('home');
   const sections = useRef<{[key: string]: HTMLElement | null}>({});
 
