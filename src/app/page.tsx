0964bebef5478b99a1a0ba7e1cfc6fa47177ffea
shangi Shangstone <patrickshangstone22@gmail.com>
2025-09-02 10:12:22 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <p class="text-pri

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -91,7 +91,7 @@ export default function Home() {
           <div className="container grid gap-12 px-4 md:px-6 lg:grid-cols-2 lg:gap-24">
             <div className="flex flex-col justify-center space-y-6">
               <div className="space-y-4">
-                <p className="text-primary font-semibold">I'm Shangi PM</p>
+                <p className="text-primary font-semibold">i'm Mushangi P</p>
                 <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
                   Software Engineer
                 </h1>
