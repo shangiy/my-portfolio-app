8f21724190cadc3bdf2d38a1d643692fc1dde766
shangi Shangstone <patrickshangstone22@gmail.com>
2025-11-21 17:22:33 +0000

    add a continous moving carousel that move in a continous motion like app

--- src/app/home-page.tsx
+++ src/app/home-page.tsx
@@ -14,6 +14,7 @@ import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { ScrollToTopButton } from '@/components/ui/scrollToTopButton';
 import { Projects } from '@/components/projects';
+import { TechCarousel } from '@/components/tech-carousel';
 
 const Footer = () => {
     return (
@@ -436,8 +437,11 @@ export default function HomePage() {
           </div>
         </section>
       </main>
+      <TechCarousel />
       <Footer />
       <ScrollToTopButton />
     </div>
   );
 }
+
+    
